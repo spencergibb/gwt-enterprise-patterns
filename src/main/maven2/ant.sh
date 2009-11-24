7803dcc6a02c895589659fec1ee09dfb86f23ba7
spudgibb@gmail.com <spudgibb@gmail.com@a98ebc78-d528-11de-bb02-e302c5db49d5>
2009-11-24 17:32:24 +0000

    mv maven2 repo to toplevel

--- src/main/maven2/ant.sh
+++
@@ -1,5 +0,0 @@
-ant install -f build-appengine.xml \
-    -Dtargetdir=./../../../target \
-    -DlocalRepository=./repo \
-    -Dmvngoal=deploy:deploy-file \
-    -Dmvnargs="-q -Durl=file://repo -DrepositoryId=local-maven2"
