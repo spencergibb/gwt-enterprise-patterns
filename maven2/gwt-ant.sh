1d59a8ebee6452baaa49112fbea103417817e17c
spudgibb@gmail.com <spudgibb@gmail.com@a98ebc78-d528-11de-bb02-e302c5db49d5>
2009-12-01 20:02:21 +0000

    updated target dirs

--- maven2/gwt-ant.sh
+++ maven2/gwt-ant.sh
@@ -1,5 +1,5 @@
 ant install -f build-gwt.xml \
-    -Dtargetdir=./../../../target \
+    -Dtargetdir=./target \
     -DlocalRepository=./repo \
     -Dmvngoal=deploy:deploy-file \
     -Dmvnargs="-q -Durl=file://repo -DrepositoryId=local-maven2"
