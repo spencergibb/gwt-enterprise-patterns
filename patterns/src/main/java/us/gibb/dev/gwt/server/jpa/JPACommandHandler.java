5c18013224da3648694ef670612927fd8cb20080
spudgibb@gmail.com <spudgibb@gmail.com@a98ebc78-d528-11de-bb02-e302c5db49d5>
2009-12-07 23:07:11 +0000

    removed warp persist (it didn't work) -- added explicit transaction handling to get persist to work

--- patterns/src/main/java/us/gibb/dev/gwt/server/jpa/JPACommandHandler.java
+++ patterns/src/main/java/us/gibb/dev/gwt/server/jpa/JPACommandHandler.java
@@ -2,8 +2,7 @@ package us.gibb.dev.gwt.server.jpa;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
-
-import com.wideplay.warp.persist.Transactional;
+import javax.persistence.EntityTransaction;
 
 import us.gibb.dev.gwt.command.Command;
 import us.gibb.dev.gwt.command.CommandException;
@@ -14,20 +13,35 @@ import us.gibb.dev.gwt.server.Context;
 public abstract class JPACommandHandler<C extends Command<R>, R extends Result> extends AbstractCommandHandler<C, R> {
     
     private EntityManagerFactory emf;
+    private boolean transactional;
 
     public JPACommandHandler(EntityManagerFactory emf) {
+        this(emf, true);
+    }
+
+    public JPACommandHandler(EntityManagerFactory emf, boolean transactional) {
         this.emf = emf;
+        this.transactional = transactional;
     }
 
     @Override
-    @Transactional
     public R execute(C command, Context context) throws CommandException {
         EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = null;
+        if (transactional) { 
+            tx = em.getTransaction();
+            tx.begin();
+        }
         try {
             return execute(em, command, context);
+        } catch (Exception e) {
+            e.printStackTrace(); //TODO log error
+            if (transactional) tx.rollback();
         } finally {
+            if (transactional) tx.commit();
             em.close();
         }
+        return null;
     }
 
     protected abstract R execute(EntityManager em, C command, Context context);
