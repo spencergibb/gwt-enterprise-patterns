59b543b0094af653b579c7c5e6b2d28de3a61c57
spudgibb@gmail.com <spudgibb@gmail.com@a98ebc78-d528-11de-bb02-e302c5db49d5>
2009-12-09 18:10:23 +0000

    added gwt 2.0.0

--- maven2/gwt-ant.sh
+++ maven2/gwt-ant.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 APP_NAME=gwt
-APP_VERSION=2.0.0-rc2
-REPO=./repo
+APP_VERSION=2.0.0
+REPO=$PWD/repo
 
 ant install -f build.xml \
     -Dapp.name=$APP_NAME \
     -Dapp.version=$APP_VERSION \
     -Dapp.distribution.location=http://google-web-toolkit.googlecode.com/files/${APP_NAME}-${APP_VERSION}.zip \
-    -Dartifacts=gwt-dev,gwt-servlet,gwt-soyc-vis,gwt-user \
+    -Dartifacts=gwt-dev,gwt-servlet,gwt-soyc-vis,gwt-user,gwt-api-checker \
     -Dgroup.id=com.google.gwt \
     -Dverify.path=${REPO}/com/google/gwt/gwt-dev/${APP_VERSION}/gwt-dev-${APP_VERSION}.jar \
     -Dtargetdir=./target \
