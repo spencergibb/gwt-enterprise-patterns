b0d933db4482b5449c35d037f7a8a6a5fcdfac23
spudgibb@gmail.com <spudgibb@gmail.com@a98ebc78-d528-11de-bb02-e302c5db49d5>
2009-12-10 20:43:19 +0000

    split LocationManager into an interface and Default impl -- added changeLocationIfNotCurrent to locationManager -- use changeLocationIfNotCurrent in HelloViewImpl to handle tab locations properly

--- patterns/src/test/java/us/gibb/dev/gwt/command/TestCommand.java
+++ patterns/src/test/java/us/gibb/dev/gwt/command/TestCommand.java
@@ -1,14 +1,14 @@
 package us.gibb.dev.gwt.command;
 
 import us.gibb.dev.gwt.event.Event;
-import us.gibb.dev.gwt.location.LocationManager;
+import us.gibb.dev.gwt.location.DefaultLocationManager;
 
 public class TestCommand implements Command<TestResult> {
     private static final long serialVersionUID = -3937874158924353940L;
     
     @SuppressWarnings("unchecked")
     public static void main(String[] args) {
-        CommandEventBus bus = new DefaultCommandEventBus(new LocationManager());
+        CommandEventBus bus = new DefaultCommandEventBus(new DefaultLocationManager());
         bus.add(new CommandEvent.Handler<TestCommand>(TestCommand.class) {
             public void handle(CommandEvent<TestCommand> event) {
                 event.getCommand();
